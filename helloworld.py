843023a8633936e3763ebf4f384c7b9112c735d6
davidkashagama1 <davidkashagama1@gmail.com>
2024-11-26 09:42:29 +0300

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,72 @@
+# birth_year = input("Enter your birth year: ")
+# age = 2024 - int(birth_year)
+# print(age)
+
+# First = float(("First: "))
+# Second= float(("Second: "))
+# sum = First + Second
+# print(sum)
+
+# course = "Python for Beginners"
+# print(course.replace("for", "4"))
+# print(course.upper())
+# print(course.find('n'))
+
+# course = 'Python for beginners'
+# print('Python' in course)
+
+# price = 26
+# print(price > 10 and price < 30)
+# print(price > 10 or price < 30)
+# print(not price > 10)
+
+# temperature = 25
+#
+# if temperature > 30:
+#     print("It's a hot day")
+#     print("Drink plenty of water")
+# elif temperature > 20:
+#     print("It's a nice day")
+#     print("Done")
+
+weight = int(input("Weight: "))
+unit = input("(K)g or (L)bs: ")
+if unit.upper()=="K":
+    converted = weight // 0.45
+    print("Weight in Lbs: " + str(converted))
+else:
+    converted = weight * 0.45
+    print("Weight in Kgs: " + str(kgs))
+
+# i = 1
+# while i <= 5:
+#     print(i)
+#     i = i+1
+#
+# names = ["John", "Bob", "Mosh", "Sam", "David"]
+# names[0] = 'Jon'
+# print(names[0:5])
+
+# numbers = [1, 2, 3, 4, 5]
+# numbers.append(6)
+# print(numbers)
+
+# numbers = [1, 2, 3, 4, 5]
+# numbers.remove(5)
+# print(numbers)
+#
+# numbers = [1, 2, 3, 4, 5]
+# print(len(numbers))
+
+# numbers = [1, 2, 3, 4, 5]
+# for item in numbers:
+#     print(item)
+#
+# i = 0
+# while i < len(numbers):
+#     print(numbers[i])
+#     i = i + 1
+
+# numbers = range(5)
+# for number in numbers:
+#     print(number )
